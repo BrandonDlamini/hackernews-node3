d73cda42c634436300e231a4cf2b1097ee2c4fb6
Brandon <brandon.dlamini95@gmail.com>
2020-11-23 12:51:07 +0200

    created update and delete Mutatio

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs =` 
-type Query {
-    info: String!
-}`
 
-const resolvers = {
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'fullstak tutorial for graphql '
+}
+]
+
+let idCount = links.length 
+
+/*const resolvers = {
     Query: {
-        info: () => `This is the API of a hackerbews Clone`
+        info: () => `This is the API of a hackerbews Clone`,
+        feed: ()=> links,
+    },
+
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+
+            return link
+        }
+    }
+}
+*/
+
+
+const resolvers ={
+    Query: {
+        link: (parent, args)=> {
+            
+            const linkFound = links.find( link => link.id === args.id);
+
+            return linkFound;
+        }
     }
 }
 
+
 const server =  new GraphQLServer({
-    typeDefs,
-    resolvers
+    typeDefs: './src/schema.graphql',
+    resolvers,
 })
 
 server.start(()=> console.log(`Server is running in http://localhost:4000`))
